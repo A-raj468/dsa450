4c6edfa7746d4c16e39ff1239e6ec8d659b40a8a
Aditya <adityaraj6508@gmail.com>
2024-07-18 12:11:46 +0530

    Naive loop based solution for arrays 5

---
+++ arrays/5/sol.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findIdx(vector<int>& arr, int value) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (value == arr[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+pair<int, int> findSimilarity(vector<int> arr1,
+                              vector<int> arr2,
+                              int n,
+                              int m) {
+    // Write Your Code here.
+    int countCommon = 0;
+    for (int i = 0; i < n; i++) {
+        if (findIdx(arr2, arr1[i]) != -1) {
+            countCommon++;
+        }
+    }
+    return pair<int, int>(countCommon, n + m - countCommon);
+}
+
+void testcase() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> arr1(n), arr2(m);
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> arr2[i];
+    }
+
+    pair<int, int> res = findSimilarity(arr1, arr2, n, m);
+
+    cout << res.first << " " << res.second << endl;
+}
+
+int main(int argc, char* argv[]) {
+    int t;
+    cin >> t;
+    while (t--) {
+        testcase();
+    }
+
+    return 0;
+}
